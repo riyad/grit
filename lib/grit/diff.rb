6c785a5afc1a4d4aa9c308cef319bdc63d823b6e
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-09-16 14:24:48 +0200

    Fixed bug in Diff#initialize where blobs where created even if the id was 000000.

--- lib/grit/diff.rb
+++ lib/grit/diff.rb
@@ -14,8 +14,8 @@ module Grit
       @b_path = b_path
       @a_sha  = a_sha =~ /^0*$/ ? nil : a_sha
       @b_sha  = b_sha =~ /^0*$/ ? nil : b_sha
-      @a_blob = a_sha.nil? ? nil : Blob.create(repo, :id => a_sha)
-      @b_blob = b_sha.nil? ? nil : Blob.create(repo, :id => b_sha)
+      @a_blob = @a_sha.nil? ? nil : Blob.create(repo, :id => @a_sha)
+      @b_blob = @b_sha.nil? ? nil : Blob.create(repo, :id => @b_sha)
       @a_mode = a_mode
       @b_mode = b_mode
       @new_file = new_file || @a_blob.nil?
