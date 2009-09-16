d25d2efe5edb644f8d5e8867eddbcb929d91062e
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-09-16 13:32:01 +0200

    Added Diff#a_sha and Diff#b_sha attributes.

--- lib/grit/diff.rb
+++ lib/grit/diff.rb
@@ -2,17 +2,20 @@ module Grit
   
   class Diff
     attr_reader :a_path, :b_path
+    attr_reader :a_sha,  :b_sha
     attr_reader :a_blob, :b_blob
     attr_reader :a_mode, :b_mode
     attr_reader :new_file, :deleted_file
     attr_reader :diff
     
-    def initialize(repo, a_path, b_path, a_blob, b_blob, a_mode, b_mode, new_file, deleted_file, diff)
+    def initialize(repo, a_path, b_path, a_sha, b_sha, a_mode, b_mode, new_file, deleted_file, diff)
       @repo = repo
       @a_path = a_path
       @b_path = b_path
-      @a_blob = a_blob =~ /^0{40}$/ ? nil : Blob.create(repo, :id => a_blob)
-      @b_blob = b_blob =~ /^0{40}$/ ? nil : Blob.create(repo, :id => b_blob)
+      @a_sha  = a_sha =~ /^0*$/ ? nil : a_sha
+      @b_sha  = b_sha =~ /^0*$/ ? nil : b_sha
+      @a_blob = a_sha.nil? ? nil : Blob.create(repo, :id => a_sha)
+      @b_blob = b_sha.nil? ? nil : Blob.create(repo, :id => b_sha)
       @a_mode = a_mode
       @b_mode = b_mode
       @new_file = new_file || @a_blob.nil?
@@ -51,7 +54,7 @@ module Grit
           deleted_file = true
         end
         
-        m, a_blob, b_blob, b_mode = *lines.shift.match(%r{^index ([0-9A-Fa-f]+)\.\.([0-9A-Fa-f]+) ?(.+)?$})
+        m, a_sha, b_sha, b_mode = *lines.shift.match(%r{^index ([0-9A-Fa-f]+)\.\.([0-9A-Fa-f]+) ?(.+)?$})
         b_mode.strip! if b_mode
         
         diff_lines = []
@@ -60,7 +63,7 @@ module Grit
         end
         diff = diff_lines.join("\n")
         
-        diffs << Diff.new(repo, a_path, b_path, a_blob, b_blob, a_mode, b_mode, new_file, deleted_file, diff)
+        diffs << Diff.new(repo, a_path, b_path, a_sha, b_sha, a_mode, b_mode, new_file, deleted_file, diff)
       end
       
       diffs
