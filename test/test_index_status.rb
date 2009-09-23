62a468d2cce4db14774a2209abe0b8a0c51f871c
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-09-23 14:48:49 +0200

    Fixed TestIndexStatus to pass.

--- test/test_index_status.rb
+++ test/test_index_status.rb
@@ -29,11 +29,13 @@ class TestIndexStatus < Test::Unit::TestCase
     Git.any_instance.expects(:diff_index).with({}, 'HEAD').returns(fixture('diff_index'))
     Git.any_instance.expects(:diff_files).returns(fixture('diff_files'))
     Git.any_instance.expects(:ls_files).with({:stage => true}).returns(fixture('ls_files'))
+    Git.any_instance.expects(:ls_files).with(:others => true).returns('')
+    Git.any_instance.expects(:ls_files).with(:others => true, :ignored => true, :exclude_standard => true).returns('')
     status = @r.status
     stat = status['lib/grit/repo.rb']
     assert_equal stat.sha_repo, "71e930d551c413a123f43e35c632ea6ba3e3705e"
     assert_equal stat.mode_repo, "100644"
-    assert_equal stat.type, "M"
+    assert_equal stat.status, :modified
   end
   
   
